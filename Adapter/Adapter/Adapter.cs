445ffd53dcc246e9d3b0debed79d5b533f9f0c01
elifoksas <elifoksas0@gmail.com>
2022-01-29 14:11:56 +0300

    adapter

--- Adapter/Adapter/Adapter.cs
+++ Adapter/Adapter/Adapter.cs
@@ -4,11 +4,39 @@ namespace Adapter
 {
     class Adapter
     {
-
-
+        interface ISiparis
+        {
+            void siparis(string s);
+        }
+        class Adapte
+        {
+            public void ozelSiparis()
+            {
+                Console.WriteLine("Gitar siparişi oluşturuldu.");
+            }
+        }
+        class Adapterr:Adapte, ISiparis
+        {
+            public void siparis(string s)
+            {
+                Console.WriteLine(s + " siparişi oluşturuldu.");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Adapte _adapte = new Adapte();
+            _adapte.ozelSiparis();
+
+            ISiparis _siparis = new Adapterr();
+            _siparis.siparis("Keman");
+
+            ISiparis _siparis2 = new Adapterr();
+            _siparis2.siparis("Tulum");
+
+            ISiparis _siparis3 = new Adapterr();
+            _siparis3.siparis("Saz");
+
+            Console.ReadKey();
         }
     }
 }
